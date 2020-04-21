399f8107a1275678233ff6660cf00ebddb579d0d
Adriano Garruba <adrianogarru@hotmail.it>
2020-04-21 09:37:17 +0200

    Conclusion

--- index.php
+++ index.php
@@ -16,35 +16,33 @@
     $products["iPhone"]=1000;
     $products["Toaster"]=75;
 
+    $taxRate=0.0825;
+
     foreach($products as $key => $value) {
       echo "<p>The ".$key." cost ".$value."</p>";
     }
 
-    echo "<h2>Items you can afford</h2>";
-
-    foreach($products as $key => $value){
-      if($value <= $credit){
-        echo "<p>The ".$key.".</p>";
-      }
-    }
-
-echo "<h1> Writing a Math Function #6 </h1>";
-
-   $amount=800;
-   $taxRate=0.0825;
-   $addedTax= $amount*$taxRate;
-   echo "<p>".$addedTax."</p>";  
-
 echo "<h1>Tax calc function </h1>";
 
 function tax_calc($amount,$tax){
-  $calculate_tax = $amount*$tax;
-  $amount = round($amount+$calculate_tax, 2);
-  return $amount;
+  $addedTax = $amount*$tax;
+  $amountwithTax = round($amount+$addedTax, 2);
+  return $amountwithTax;
 }
-echo tax_calc(750, 0.223);
 
+foreach ($products as $key => $value){
+  $costwithtax = tax_calc($value, $taxRate);
+  echo "<p> The ".$key." costs ".$costwithtax." with tax </p>";
+}
 
+echo "<h2>Items you can afford</h2>";
+
+foreach($products as $key => $value){
+  $costwithtax = tax_calc($value, $taxRate);
+  if($costwithtax <= $credit){
+    echo "<p>".$key."</p>";
+  }
+}
     ?>
   </body>
 </html>
